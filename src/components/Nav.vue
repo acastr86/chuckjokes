4cc2b3ac644c97347645a7284cbc1ae26bc84433
Adonis <acastr86@stu.southtexascollege.edu>
2021-11-17 20:59:32 -0600

    Custom Changes

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,25 +1,10 @@
 <template>
-<div class="bg-blue-800 text-blue-200">
-    <div class= "container mx-auto flex items-center justify-between">
-        <h1 class="tracking-tighter text-3xl font-thin">
-            Basic<span class="font-normal">Router</span>
+<div class="text-blue-200 bg-blue-800">
+    <div class= "container flex items-center justify-center mx-auto">
+        <h1 class="py-8 text-3xl font-thin tracking-tighter ">
+            Chuck<span class="font-normal">Jokes</span>
         </h1>
-        <nav>
-            <ul class="flex space-x-4">
-                <router-link to="/">
-                <li class="py-8 px-4 hovver:cursor-pointer hover:bg-blue-500 hover:text-blue-800">Home</li>
-                </router-link>
-                <router-link :to="{name:'About'}">
-                <li class="py-8 px-4 hovver:cursor-pointer hover:bg-blue-500 hover:text-blue-800">About</li>
-                </router-link>
-                <router-link :to="{path: '/news', name:'News'}">
-                <li class="py-8 px-4 hovver:cursor-pointer hover:bg-blue-500 hover:text-blue-800">News</li>
-                </router-link>
-                 <router-link :to="{path: '/contact'}">
-                <li class="py-8 px-4 hovver:cursor-pointer hover:bg-blue-500 hover:text-blue-800">Contact</li>
-                </router-link>
-            </ul>
-        </nav>
+        
     </div>
 </div>
     
